7d35f981eac433632eb686cd63b7445391b2800c
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-07 22:27:14 +0300

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - it prints a string of output
  *
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - it prints a string of output
  *
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
