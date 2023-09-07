e5251789394c82a04d43086ab90c83a6e32c59b9
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-07 22:07:31 +0300

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - it prints a string of output
  *
  * Return: Always 0 
  */
-int main(void){
+int main(void)
+{
     puts("with proper grammar, but the outcome is a piece of art,\n");
     
-    return 0;
-
+    return (0);
 }
