8347f98b0c663c2d715f85edfa305c821724cb1d
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-07 23:13:27 +0300

    my commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - it prints a string of output
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
