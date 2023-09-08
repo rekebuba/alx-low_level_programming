725e2aab908819c613338c91a990d467e794a261
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-08 23:07:17 +0300

    my commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
- * main - it prints a string of output
+ * main - it prints all single digit numbers of base 10.
  *
  * Return: Always 0
  */
 int main(void)
 {
+int i = '0';
+int x = 'a';
+while (i <= '9')
+{
+putchar(i);
+i++;
+}
+while (x <= 'f')
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
+}
