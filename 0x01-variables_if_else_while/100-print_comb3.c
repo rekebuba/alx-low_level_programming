d51182221faee0641852ed3b0ba96714cc7cef0c
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-08 23:51:53 +0300

    my commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 /**
- * main - it prints a string of output
- *
+ * main - it prints different combinations of two digits.
+ * 
  * Return: Always 0
  */
 int main(void)
 {
+int i, j;
+for (i = '0'; i <= '8'; i++)
+{
+for (j = '1'; j <= '9'; j++)
+{
+putchar(i);
+putchar(j);
+if (i != '8' || j != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
