acdd6b0f2eee212213b5636e2e75896133e07365
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-09 01:24:48 +0300

    my commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 /**
- * main - it prints a string of output
+ * main - it prints different combinations of two digits.
  *
  * Return: Always 0
  */
 int main(void)
 {
+int a, b;
+for (a = 0; a <= 98; a++)
+{
+for (b = a+1; b <= 99; b++)
+{
+
+putchar((a / 10) + '0');
+putchar((a % 10) + '0');
+putchar(' ');
+putchar((b / 10) + '0');
+putchar((b % 10) + '0');
+
+if (a != 98 || b != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
