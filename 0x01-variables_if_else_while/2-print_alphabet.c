92baf843f88c2fb36051aae0e4199ec50326e32b
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-08 21:51:01 +0300

    my commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 /**
- * main - it prints a string of output
+ * main - prints the alphabet in lowercase.
  *
  * Return: Always 0
  */
 int main(void)
 {
 char letter = 'a';
+char newline = '\n';
 while (letter <= 'z')
 {
 putchar(letter);
 letter++;
 }
+putchar(newline);
 return (0);
 }
