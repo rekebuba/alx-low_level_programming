7e1b618eee476861c808656dd95bdefef5753e6f
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-09 00:14:04 +0300

    my commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@ int main(void)
 int i, j;
 for (i = '0'; i <= '8'; i++)
 {
-for (j = '1'; j <= '9'; j++)
+for (j = i + 1; j <= '9'; j++)
 {
 putchar(i);
 putchar(j);
@@ -19,6 +19,7 @@ putchar(',');
 putchar(' ');
 }
 }
+j = '2';
 }
 putchar('\n');
 return (0);
