9c62513f9ebf37bb03909df602e2f7443a1fd2f8
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-13 21:07:04 +0300

    my commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main - prints the alphabet, in lowercase
+ * main - make the alphabet.
  *
  * Return: Always 0.
  */
-void print_alphabet(void) /*print*/
+void print_alphabet(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
