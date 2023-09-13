36313970b113827230224d3212fc16d327dcdbc3
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-13 21:11:11 +0300

    my commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,9 +4,17 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-void print_alphabet_x10(void);
+char ch;
+int i;
+for (i = 0; i < 10; i++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
 _putchar('\n');
+}
 return (0);
 }
