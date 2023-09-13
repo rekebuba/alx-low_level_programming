6ba83251f196ac68030cc825523e41b4de0affb5
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-13 21:36:57 +0300

    my commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,11 +11,11 @@ for (a = 0; a <= 23; a++)
 {
 for (b = 0; b <= 59; b++)
 {
-putchar((a / 10) + '0');
-putchar((a % 10) + '0');
-putchar(':');
-putchar((b / 10) + '0');
-putchar((b % 10) + '0');
+_putchar((a / 10) + '0');
+_putchar((a % 10) + '0');
+_putchar(':');
+_putchar((b / 10) + '0');
+_putchar((b % 10) + '0');
 }
 }
 }
