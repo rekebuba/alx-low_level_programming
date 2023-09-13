60662396731794700db4f72c50b64b1aeeecb9ab
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-13 23:31:51 +0300

    my commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  the 9 times table
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int z = 0;
+    int sum = 0;
+    int i = 1023;
+    for (i = 1023; i >= 0; i--)
+    {
+        if ((i % 3) == 0 || (i % 5) == 0)
+        {
+            sum += i;
+        }
+    }
+    printf("%d", sum);
+}
