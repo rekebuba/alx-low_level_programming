7e541c94791d10e414d6586d09f510af8558ccaa
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-14 00:55:38 +0300

    my commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,11 +16,11 @@ for (mult = 1; mult <= n; mult++)
 _putchar(',');
 _putchar(' ');
 prod = num * mult;
-if (prod <=99)
+if (prod <= 99)
 _putchar(' ');
-if (prod <=9)
+if (prod <= 9)
 _putchar(' ');
-if (prod >=100)
+if (prod >= 100)
 {
 _putchar((prod / 100) + '0');
 _putchar((prod / 10) % 10 + '0');
@@ -29,7 +29,7 @@ else if (prod <= 99 && prod >= 10)
 {
 _putchar((prod / 10) + '0');
 _putchar((prod % 10) + '0');
-}    
+}
 }
 }
 }
--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main -  the 9 times table
  *
@@ -7,17 +6,22 @@
  */
 int main(void)
 {
-    int sum;
-    int add;
-    int i;
-    for (i = 1; i <= 50; i++)
-    {
-        if (i == 1 || i == 2)
-        { 
-            printf("%d, ", i);
-        }
-        sum = i + (i - 1);
-        add = sum + (i - 1);       
-        printf("%d, ", add);
-    } 
+int i = 0;
+long j = 1, k = 2;
+while (i < 50)
+{
+if (i == 0)
+printf("%ld", j);
+else if (i == 1)
+printf(", %ld", k);
+else
+{
+k += j;
+j = k - j;
+printf(", %ld", k);
+}
+++i;
+}
+printf("\n");
+return (0);
 }
