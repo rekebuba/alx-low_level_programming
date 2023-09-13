c7e6d9dbfbc305f999ed5a115d79240b1b78dcb3
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-13 22:04:47 +0300

    my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,22 @@
  */
 void times_table(void)
 {
-int i, k, j;
+int i, k, j, m;
 for (i = 0; i <= 9; i++)
 {
 for (k = 0; k <= 9; k++)
 {
 j = (i * k) + '0';
 _putchar(j);
+if (j > 9)
+{
+m = j - (j % 10) / 10;
+_putchar("m")
+}
 if (j != 81)
 {
 _putchar(',');
+_putchar(' ');
 _putchar(' ');
 }
 }
