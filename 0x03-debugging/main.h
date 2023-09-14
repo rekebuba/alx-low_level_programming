0544d70f4594767f389e7cb81adf0471829d8d2b
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-14 20:40:38 +0300

    Merge branch 'master' of https://github.com/rekebuba/alx-low_level_programming

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,38 +3,6 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i)
-{
-if (i < 0)
-{
-printf("%d is negative\n", i);
-}
-else if (i > 0)
-{
-printf("%d is positive\n", i);
-}
-else if (i == 0)
-{
-printf("%d is zero\n", i);
-}
-}
-int largest_number(int a, int b, int c)
-{
-int largest;
+void positive_or_negative(int i);
 
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (b > a && b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-
-return (largest);
-}
 #endif /* MAIN_H */
