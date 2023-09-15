b6e86d8abc1118f71d00300e7f902e257c6121cc
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-15 23:51:12 +0300

    Merge branch 'master' of https://github.com/rekebuba/alx-low_level_programming

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,14 +7,17 @@
  */
 void print_square(int size)
 {
-int k = 0;
+int k, j;
 if (size > 0)
+{
+for (j = 0; j < size; j++)
 {
 for (k = 0; k < size; k++)
 {
 _putchar('#');
 }
 _putchar('\n');
+}
 }
 
 else if (size <= 0)
