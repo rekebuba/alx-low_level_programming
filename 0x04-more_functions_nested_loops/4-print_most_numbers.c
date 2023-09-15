09e60b9b3eb615196f42bbe5ab74370c318c383b
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-15 13:52:55 +0300

    Merge branch 'master' of https://github.com/rekebuba/alx-low_level_programming

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_numbers - tests function that prints if integer is positive or negative
+* print_most_numbers - tests function that prints if integer is positive or negative
 * Return: 0
 */
 void print_most_numbers(void)
