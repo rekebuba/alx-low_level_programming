3d47e0e446494f94ebc328b2375964e9949810d4
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-16 00:00:49 +0300

    Merge branch 'master' of https://github.com/rekebuba/alx-low_level_programming

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,24 +7,24 @@
  */
 int main (void)
 {
-    int i;
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0)
-        {
-            printf("Fizz ");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz ");
-        }
-        else if (i % 3 == 0 && i % 5 ==0)
-        {
-            printf("FizzBuzz ");
-        }
-        else
-        {
-            printf("%d ", i);
-        }
-    }
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (i % 3 == 0 && i % 5 ==0)
+{
+printf("FizzBuzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
 }
