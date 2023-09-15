6839564e6e5b656ea698cca9e6a565f4643c40ea
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-16 00:58:02 +0300

    Merge branch 'master' of https://github.com/rekebuba/alx-low_level_programming

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,15 @@ void print_most_numbers(void)
 char i = '0';
 while (i <= '9')
 {
-if (i != '2' || i != '4')
+if (i == '2' || i == '4')
 {
-_putchar(i + '0');
-i++;
+    continue;
 }
+    else
+    {
+        _putchar(i + '0');
+    }
+i++;
 }
 _putchar('\n');
 }
