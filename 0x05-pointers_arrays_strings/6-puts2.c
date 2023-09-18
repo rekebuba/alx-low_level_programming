3333c30343e099648ef3027c107e12ca983c7f2e
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-18 17:06:05 +0300

    my commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,15 @@ void puts2(char *str)
 {
 int i = 0;
 int len = strlen(str);
-for (i = 0; i <= len; i + 2)
+for (i = 0; i <= len; i++)
+{
+if (i = 0)
 {
 putchar(str[i]);
+}
+else
+{
+putchar(str[i+1]);
+}
 }
 }
