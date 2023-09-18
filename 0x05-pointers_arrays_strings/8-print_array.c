8b6695ec2048210fa33338bee3afed807c327624
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-18 19:07:40 +0300

    my commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+* print_array - prints half of a string, followed by a new line.
+* @a: is string
+* @n: is integer.
 * Return: 0
+*/
 
 void print_array(int *a, int n)
 {
-
+int k;
+for (k = 0; k <= n; k++)
+{
+printf("%d", a[k]);
+if (k != n)
+{
+printf(", ");
+}
+}
+printf("\n");
 }
