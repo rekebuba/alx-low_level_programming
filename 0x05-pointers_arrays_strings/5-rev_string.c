4b0d3ded5b461b846f01385aca02e25c815faf3a
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-18 16:45:25 +0300

    my commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
 * rev_string - reverses a string.
@@ -8,5 +9,12 @@
 
 void rev_string(char *s)
 {
-
+int len = strlen(s);
+int i;
+char cha[strlen(s) + 1];
+for (i = len - 1; i >= 0; i--)
+{
+cha[i] = s[i];
+}
+puts(cha);
 }
