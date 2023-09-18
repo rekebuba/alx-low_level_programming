97fa1d427fb34b6c9723113e3ab4b571eeefa458
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-18 18:00:11 +0300

    my commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,14 +13,14 @@ int i;
 int len = strlen(str);
 if (len % 2 != 0)
 {
-for (i = len / 2; i < len; i++)
+for (i = len / 2; i <= len; i++)
 {
 putchar(str[i]);
 }
 }
 if (len % 2 == 0)
 {
-for (i = (len - 1) / 2; i < len; i++)
+for (i = (len - 1) / 2; i <= len; i++)
 {
 putchar(str[i]);
 }
