44ba769218361ee18bc509b71c308362c2e89c36
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-20 21:12:27 +0300

    my commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    if (strcmp(s1, s2) == 0)
-    {
-        return (0);
-    }
-    else if (strcmp(s1, s2) < 0)
-    {
-        return (-15);
-    }
-    else if (strcmp(s1, s2) > 0)
-    {
-        return (15);
-    }
-    return(0);
+if (strcmp(s1, s2) == 0)
+{
+return (0);
+}
+else if (strcmp(s1, s2) < 0)
+{
+return (-15);
+}
+else if (strcmp(s1, s2) > 0)
+{
+return (15);
+}
+return(0);
 }
