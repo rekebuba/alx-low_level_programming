13beae0c277e291ad680e3919f7543088b0a6e13
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-20 20:58:13 +0300

    my commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -15,13 +15,7 @@ int t;
 for (i = 0; i < n--; i++)
 {
 t = a[i];
-//This line assigns the value at index i of the array a to the variable t. It temporarily stores the value for swapping.
 a[i] = a[n];
-// This line assigns the value at index n of the array a to the element at index i. It swaps the value at index i with the value at index n.
 a[n] = t;
-//This line assigns the value stored in the variable t to the element at index n of the array a. It completes the swap by assigning the original value of a[i] to a[n]
-/**Note: The n-- in the loop condition is a post-decrement operation, which means that n is decremented after each iteration of the loop. 
- *This ensures that the loop iterates from the first element to the middle element of the array, effectively reversing the array.
- */
 }
 }
