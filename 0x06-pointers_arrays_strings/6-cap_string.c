114b7feab10df399f3e4c1685a5410ae03398325
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-21 20:31:48 +0300

    my commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,39 @@
 #include "main.h"
 
 /**
-* cap_string - updates the value it points to to 98.
-* Return: 0
-* @n: character.
-*/
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *cap_string(char *str)
+{
+int index = 0;
 
-char *cap_string(char *n)
+while (str[index])
 {
-int i;
-int len = strlen(n);
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
 
-for (i = 0; i < len; i++)
-{
-if (isalpha(n[i]))
-{
-n[i] = n[i] - 32;
+if 
+(str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}' ||
+index == 0)
+
+str[index] -= 32;
+index++;
 }
-}
-return (n);
+
+return (str);
 }
