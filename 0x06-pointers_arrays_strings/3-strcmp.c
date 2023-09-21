f99d19fedd9fb3c4ebfee74babd81ad4af73034d
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-21 19:29:16 +0300

    my commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
 * _strcmp - compares two strings.
@@ -10,17 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-if (strcmp(s1, s2) == 0)
+int i;
+
+i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
 {
-return (0);
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
 }
-else if (strcmp(s1, s2) < 0)
-{
-return (-15);
-}
-else if (strcmp(s1, s2) > 0)
-{
-return (15);
+i++;
 }
 return (0);
 }
