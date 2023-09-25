1ca42a13ebb4130e4fc82cf01d433ed51ba696d4
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-25 22:43:37 +0300

    my commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,13 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i;
-for (i = 0; i < n; n++)
+int r = 0;
+int i = n;
+
+for (; r < i; r++)
 {
-dest[i] = src[i];
+dest[r] = src[r];
+n--;
 }
 return (dest);
 }
