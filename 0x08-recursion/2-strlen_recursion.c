2cbfcb007a02c6d1fcb3dc4e08783e85a25cc85c
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-29 22:23:29 +0300

    my commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,9 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-if (*s != '\0')
+int sum;
+sum = 0;
+if (*s)
 {
-_strlen_recursion(s + 1);
+sum++;
+sum += _strlen_recursion(s + 1);
 }
-return ();
+return (sum);
 }
