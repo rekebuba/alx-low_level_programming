4dc1430a242d195b7e28888de9ff4e069912baac
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-29 19:46:40 +0300

    my commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - Write a function that returns the factorial of a given number.
+ * @n: string
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n != 0)
+{
+n = n * factorial(n - 1);
+}
+if (n == 0)
+{
+return (1);
+}
+return (n);
+}
