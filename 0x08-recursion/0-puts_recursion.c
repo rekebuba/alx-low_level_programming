d0ff37d6f2e77f6e1f19305704f1f73cc51db2b2
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-09-29 15:03:51 +0300

    my commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- * 
  * _puts_recursion: prints a string, followed by a new line.
- * 
  * @param s: string.
  */
 void _puts_recursion(char *s)
 {
 _putchar(*s);
-s ++;
+s++;
 if (*s != '\0')
 {
 _puts_recursion(s);
