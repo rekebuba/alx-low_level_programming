09fe8773d411f150e358da8a57568d8c16872fd0
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-02 17:09:00 +0300

    my commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,13 +18,13 @@ if (argc - 1 == 0)
 printf("0\n");
 return (1);
 }
-for (i = 0; i < argc; i++)
+for (i = 1; i < argc; i++)
 {
 if (isdigit(*argv[i]))
 {
 sum += atoi(argv[i]);
 }
-else
+if (!isdigit(*argv[i]))
 {
 printf("Error\n");
 return (1);
