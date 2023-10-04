411a8840960c6844567b7cea5f1a1bc87ee1dbac
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-04 12:16:34 +0300

    my commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@
 int **alloc_grid(int width, int height)
 {
 int **grid;
-int i, j; 
+int i, j;
 if (width <= 0 || height <= 0)
 {
 return (NULL);
@@ -32,13 +32,13 @@ for (j = 0; j < i; j++)
 free(grid[j]);
 }
 free(grid);
-return NULL;
+return (NULL);
 }
 for (j = 0; j < width; j++)
 {
 grid[i][j] = 0;
 }
 }
-return grid;
+return (grid);
 free(grid);
 }
