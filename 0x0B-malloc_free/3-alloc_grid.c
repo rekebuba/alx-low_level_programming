f274ee1275e43b091bf333d5ccfdd8e4b59e93a6
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-04 22:46:00 +0300

    my commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,38 +11,35 @@
 
 int **alloc_grid(int width, int height)
 {
-int **grid;
-int i, j;
+int **array;
+int i, y;
+
 if (width <= 0 || height <= 0)
+return (NULL);
+
+array = malloc(sizeof(int *) * height);
+
+if (array == NULL)
+return (NULL);
+
+for (i = 0; i < height; i++)
 {
+array[i] = malloc(sizeof(int) * width);
+
+if (array[i] == NULL)
+{
+for (; i >= 0; i--)
+free(array[i]);
+
+free(array);
 return (NULL);
 }
-grid = (malloc(sizeof(int) * height));
-if (grid == NULL)
-{
-return (NULL);
 }
 
 for (i = 0; i < height; i++)
 {
-grid[i] = (int *) (malloc(sizeof(int) * width));
-if (grid[i] == NULL)
-{
-for (; i >= 0; i--)
-{
-free(grid[i]);
+for (y = 0; y < width; y++)
+array[i][y] = 0;
 }
-free(grid);
-return (NULL);
-}
-}
-for (i = 0; i < height; i++)
-{
-for (j = 0; j < width; j++)
-{
-grid[i][j] = 0;
-}
-}
-return (grid);
-free(grid);
+return (array);
 }
