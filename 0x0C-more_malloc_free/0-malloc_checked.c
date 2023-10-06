c5e783a5e3ec9be494ec56d9b34e78adfe8fc29e
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-06 14:27:20 +0300

    my commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: unsigned int.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *array;
+array = malloc(b);
+if (array == NULL)
+{
+exit (98);
+}
+return array;
+}
