de8dfd89d97d52191084e7173d8c9c0eb38ac8d9
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-09 22:54:24 +0300

    my commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_dog(struct dog *d)
+{
+    d{
+    name = name;
+    age = age;
+    owner = owner;
+    };
+    
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef HEADER
+#define HEADER
+/**
+ * dog - short description
+ * @name: name of yhe person
+ * @age: their age
+ * @owner: owner
+ * 
+ * description: long description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner; 
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+#endif
