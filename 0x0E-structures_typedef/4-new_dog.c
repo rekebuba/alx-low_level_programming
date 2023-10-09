7e97cb39cd965b36fd033c3ea5bd6589dd51cae9
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-10 00:27:52 +0300

    my commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,13 +13,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 int len1 = 0, len2 = 0, i;
 dog_t *ptr;
-
-ptr = malloc(sizeof(dog_t));
-if (ptr == NULL)
-{
-return (NULL);
-}
-
 while (name[len1] != '\0')
 {
 len1++;
@@ -28,11 +21,23 @@ while (owner[len2] != '\0')
 {
 len2++;
 }
+
+ptr = malloc(sizeof(dog_t));
+if (ptr == NULL)
+{
+return (NULL);
+}
 ptr->name = malloc(sizeof(char) * (len1 + 1));
-ptr->owner = malloc(sizeof(char) * (len2 + 1));
-if (ptr->name == NULL || ptr->owner == NULL)
+if (ptr->name == NULL)
 {
 free(ptr);
+return (NULL);
+}
+ptr->owner = malloc(sizeof(char) * (len2 + 1));
+if ( ptr->owner == NULL)
+{
+free(ptr);
+free(ptr->name);
 return (NULL);
 }
 for (i = 0; i < len1; i++)
