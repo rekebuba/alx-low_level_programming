acb1869b722d0ca66283fd92369729dca7ca60be
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-11 23:55:35 +0300

    my commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,12 +2,13 @@
 
 /**
  * print_name - prints a name.
- * 
  * @name: CHARACTER
  * @f: function
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (name == NULL || f == NULL)
+return;
 f(name);
 }
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,24 +2,25 @@
 #include <string.h>
 #include "3-calc.h"
 
+/**
+ * get_op_func - Get the op func object
+ * @s: character
+ * Return: int(*)(int, int)
+ */
+
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
-    while (ops[i].op != NULL)
-    {
-        if (strcmp(ops[i].op, s) == 0)
-        {
-            return (ops[i].f);
-        }
-        i++;
-    }
-    return (NULL);
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (ops[i].op != NULL && *(ops[i].op) != *s)
+i++;
+
+return (ops[i].f);
 }
