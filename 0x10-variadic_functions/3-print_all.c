6f502d8fbc6befe189d5580996d9321c7e50a23b
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-13 02:11:19 +0300

    my commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -16,34 +16,34 @@ va_list(arg);
 va_start(arg, format);
 if (format)
 {
-    while (format[len] != '\0')
-    {
-        switch (format[len])
-        {
-        case 'c':
-            printf("%s%c", step, va_arg(arg, int));
-            break;
-        case 'i':
-            printf("%s%d", step, va_arg(arg, int));
-            break;
-        case 'f':
-            printf("%s%f", step, va_arg(arg, double));
-            break;
-        case 's':
-            str = va_arg(arg, char *);
-            if (str == NULL)
-            {
-                printf("nil");
-            }
-            printf("%s%s", step, str);
-            break;
-        default:
-            len++;
-            continue;
-        }
-        step = ", ";
-        len++;
-    }
+while (format[len] != '\0')
+{
+switch (format[len])
+{
+case 'c':
+printf("%s%c", step, va_arg(arg, int));
+break;
+case 'i':
+printf("%s%d", step, va_arg(arg, int));
+break;
+case 'f':
+printf("%s%f", step, va_arg(arg, double));
+break;
+case 's':
+str = va_arg(arg, char *);
+if (str == NULL)
+{
+printf("nil");
+}
+printf("%s%s", step, str);
+break;
+default:
+len++;
+continue;
+}
+step = ", ";
+len++;
+}
 }
 printf("\n");
 va_end(arg);
