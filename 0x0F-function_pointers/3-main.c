40efba7a8d0e35198d98087a58b98483605218f5
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-14 00:29:38 +0300

    my commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,7 +6,6 @@
  * main - Prints the result of simple operations.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
- *
  * Return: Always 0.
  */
 
@@ -19,7 +18,6 @@ if (argc != 4)
 printf("ERROR\n");
 return (98);
 }
-
 num1 = atoi(argv[1]);
 op = argv[2];
 num2 = atoi(argv[3]);
@@ -28,15 +26,12 @@ if (get_op_func(op) == NULL || op[1] != '\0')
 printf("Error\n");
 exit(99);
 }
-
 if ((*op == '/' && num2 == 0) ||
 (*op == '%' && num2 == 0))
 {
 printf("Error\n");
 exit(100);
 }
-
 printf("%d\n", get_op_func(op)(num1, num2));
-
 return (0);
 }
