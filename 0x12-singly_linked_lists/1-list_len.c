670a6a66518c5f5822ee36372d837dc85c165bb8
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-19 18:28:30 +0300

    my commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,7 +4,7 @@ size_t list_len(const list_t *h)
 {
     const list_t *current = h;
     int count = 0;
-    while (current->str != NULL)
+    while (current != NULL)
     {
         current = current->next;
         count++;
