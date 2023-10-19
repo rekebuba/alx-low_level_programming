7d7db8e3fccf42b4783bc2fdbd5b99ff8b9f5ed4
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-19 23:39:44 +0300

    my commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,20 +8,13 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-char *duplicate;
 list_t *new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 {
 return (NULL);
 }
-duplicate = strdup(str);
-if (duplicate == NULL)
-{
-return (NULL);
-free(new_node);
-}
-new_node->str = duplicate;
-new_node->len = strlen(duplicate);
+new_node->str = strdup(str);
+new_node->len = strlen(str);
 new_node->next = *head;
 *head = new_node;
 return (new_node);
