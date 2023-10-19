7b373af6dcfa623e0eea116dd43b7c53913737da
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-19 21:08:11 +0300

    my commit

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,12 +2,14 @@
 
 void free_list(list_t *head)
 {
-    list_t *temp = head;
-    while (temp != NULL)
+    list_t *current = head;
+    list_t *next;
+
+    while (current != NULL)
     {
-        head = temp->next;
-        free(temp->str);
-        free(temp);
-        temp = head;
+        next = current->next;
+        free(current->str);
+        free(current);
+        current = next;
     }
 }
