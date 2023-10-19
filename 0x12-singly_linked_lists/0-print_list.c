5344687b1379c84024da6b0f7c070a8108e93c56
rekebuba <abukiabdullahi1@gmail.com>
2023-10-19 07:45:06 -0700

    my commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,20 +8,20 @@
  */
 size_t print_list(const list_t *h)
 {
-    const list_t *current = h;
-    size_t count = 0;
-    while (current != NULL)
-    {
-        if (current->str == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%u] (%s)\n", current->len, current->str);
-        }
-        current = current->next;
-        count++;
-    }
-    return (count);
+	const list_t *current = h;
+	size_t count = 0;
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+	    		printf("[%u] (%s)\n", current->len, current->str);
+		}
+		current = current->next;
+		count++;
+	}
+	return (count);
 }
