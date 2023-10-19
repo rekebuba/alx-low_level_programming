e90b24ff8e43bcdb2ca615dedd05b4cafba09d2e
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-19 23:42:14 +0300

    my commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,27 @@
-section .data
-    format db "Hello, Holberton", 10, 0   ; The format string for printf
+extern printf; the C function, to be called
 
-section .text
-    extern printf                       ; Declare the printf function as external
 
-global _start
-_start:
-    sub rsp, 8                          ; Align the stack
+      section.data; Data section, initialized variables
+msg:    db "Hello, Holberton", 0; C string needs 0
+fmt:    db "%s", 10, 0; The printf format, "\n", '0'
 
-    lea rdi, [rel format]                ; Load the address of the format string into rdi
-    xor eax, eax                        ; Set eax to 0 (no floating-point arguments)
-    call printf                         ; Call the printf function
 
-    add rsp, 8                          ; Restore the stack
+       section.text; Code section.
 
-    mov eax, 60                         ; Exit syscall number
-    xor edi, edi                        ; Exit status: 0
-    syscall                             ; Invoke the syscall to exit
+
+   global main; the standard gcc entry point
+   main:; the program label for the entry point
+   push    rbp     ; set up stack frame, must be alligned
+
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0; or can be xor rax, rax
+   call printf; Call C function
+
+
+   pop rbp; restore stack
+
+
+   mov rax,0; normal, no error, return value
+   ret; return
