9716c96288bcf461033f7e1dc0769c298dee63da
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-19 22:47:16 +0300

    cy commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,9 +15,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -25,7 +25,9 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void first (void) __attribute__((constructor));/*this command allow it to execute before main*/
-
+/**
+ * first - this command allow it to execute before main
+*/
+void first(void) __attribute__((constructor));
 
 #endif
