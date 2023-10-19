3186fcf60fcd6d6fe378b5a9d351cd626a83c556
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-19 17:30:29 +0300

    my commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to struct list_t 
+ * Return: size_t 
+ */
+size_t print_list(const list_t *h)
+{
+const list_t *current = h;
+size_t count = 0;
+while (current != NULL)
+{
+    if (current->str == NULL)
+    {
+        printf("[0] (nil)\n");
+    }
+    else
+    {    
+        printf("[%u] (%s)\n", current->len, current->str);
+    }
+    current = current->next;
+    count++;
+}
+return (count);
+}
