57945f270a3a4cb9728002cb3657a1a168405575
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-23 14:40:36 +0300

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,16 +3,16 @@
 /**
  * print_listint - prints all elements of listint_t
  * @h: struct type
- * @return size_t (the number of nodes)
+ * Return: size_t (the number of nodes)
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
-    while (h != NULL)
-    {
-        printf("%d", h->n);
-        h = h->next;
-        count++;
-    }
-    return (count);
+size_t count = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
+return (count);
 }
