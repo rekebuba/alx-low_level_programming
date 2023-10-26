f673cea1aa2be045a04ecaf3317d3fd771c1cf0c
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-26 21:21:31 +0300

    0x14-bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,9 +4,26 @@
  * get_bit - Get the bit object
  * @n:
  * @index: starting from 0 of the bit you want to get
- * Return: the value of the bit at index index or -1 if an error occured 
+ * Return: the value of the bit at index index or -1 if an error occurred 
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-
+    char buffer[64];
+    unsigned int idx = 0;
+    while (n != 0)
+    {
+        buffer[idx] = (n & 1)  ?  '1' : '0';
+        n = (n >> 1);
+        idx++;
+    }
+    if (index > idx)
+    {
+        return (-1);
+    }
+    while (idx != index)
+    {
+        idx++;
+    }
+    _putchar(buffer[idx]);
+    return (1);
 }
