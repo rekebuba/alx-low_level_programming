58e8352754ff056acfd718224b8500835b78dba5
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-27 00:07:17 +0300

    0x14-bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,11 +10,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned long int mask;
 unsigned long int bit;
-if (index >= sizeof(unsigned long int) * 8) 
+if (index >= sizeof(unsigned long int) * 8)
 {
 return (-1);
 }
-
 mask = 1UL << index;
 bit = (n & mask) >> index;
 
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,7 +3,7 @@
 /**
  * set_bit - Set the bit object
  * @n: unsigned long int
- * @index:
+ * @index: a place holder
  * Return: 1 if it worked, or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
