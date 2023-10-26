b3eefd46c0f1786748fee629ee02fcc9b2cefe6d
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-27 01:11:06 +0300

    0x14-bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
  * get_bit - Get the bit object
  * @n: unsigned long int
  * @index: starting from 0 of the bit you want to get
- * Return: the value of the bit at index index or -1 if an error occurred 
+ * Return: the value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
