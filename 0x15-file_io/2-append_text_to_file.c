728884b2d1c38dc563e5e5d98cf76ef8d21c2558
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-30 23:59:53 +0300

    0x15-file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,12 +18,12 @@ return (count);
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int file;
+    int file , write_file;
     if (filename == NULL)
     {
         return (-1);
     }
-    file = open(filename, O_RDONLY);
+    file = open(filename, O_RDONLY | O_APPEND, 0600);
     if (file == -1)
     {
         return (-1);
@@ -31,8 +31,13 @@ int append_text_to_file(const char *filename, char *text_content)
     lseek(file, 0L, SEEK_END); /* Go to the end of the file*/
     if (text_content == NULL)
     {
-        return (1);
+        return (-1);
     }
-    write(file, text_content, _strlen(text_content));
+    write_file = (file, text_content, _strlen(text_content));
+    if (write_file == -1)
+    {
+        return (-1);
+    }
+    close(file);
     return (1);
 }
