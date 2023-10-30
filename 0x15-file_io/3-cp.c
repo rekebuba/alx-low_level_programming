b16c877297072cc4b581b99678f821231d699dea
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-30 23:28:10 +0300

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,68 +10,74 @@ void free_close(char **buf, int *fd1, int *fd2);
 */
 int main(int argc, char *argv[])
 {
-    int file1, file2, to_read, to_write;
-    char *file_from, *file_to, *buffer;
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        return (97);
-    }
-    file_from = argv[1];
-    file_to = argv[2];
-    file1 = open(file_from, O_RDONLY);
-    if (file1 == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-        return (98);
-    }
-    file2 = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
-    if (file2 == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-        return (99);
-    }
-    buffer = malloc(sizeof(char) * 1024);
-    if (buffer == NULL)
-        return (-1);
-    to_read = read(file1, buffer, 1024);
-    if (to_read == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-        return (98);
-    }
-    to_write = write(file2, buffer, to_read);
-    if (to_write != to_read)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", file_to);
-        return (99);
-    }
-    free_close(buffer, file1, file2);
-    return (0);
+	int i = 0;
+	int n, n1, fd_from, fd_to, *fd1_ptr = &fd_from, *fd2_ptr = &fd_to;
+	char *buf, **buf_ptr = &buf;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	buf = malloc(sizeof(char) * 1024);
+	if (!buf)
+		return (-1);
+
+	do {
+		n = read(fd_from, buf, 1024);
+		if (n == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		n1 = write(fd_to, buf, n);
+		if (n1 != n)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+		i++;
+	} while (n == 1024);
+
+	free_close(buf_ptr, fd1_ptr, fd2_ptr);
+
+	return (0);
 }
 
 /**
 * free_close - free malloc'd memory and closes opened files
-* @buffer: pointer to the string buffer
-* @file1: pointer to the file1
-* @file2: pointer to file2
+* @buf: pointer to the string buf
+* @fd1: pointer to the fd_from
+* @fd2: pointer to fd_to
 * Return: returns nothing
 */
-void free_close(char **buffer, int *file1, int *file2)
+void free_close(char **buf, int *fd1, int *fd2)
 {
 	int n, n1;
 
-	free(*buffer);
-	n = close(*file1);
+	free(*buf);
+	n = close(*fd1);
 	if (n == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *file1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *fd1);
 		exit(100);
 	}
-	n1 = close(*file2);
+	n1 = close(*fd2);
 	if (n1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *file2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *fd2);
 		exit(100);
 	}
 }
