14906bf0d10c2eae6a25d1c85b8af6e2f8fad0e0
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-31 00:28:34 +0300

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,21 +26,24 @@ return (count);
 */
 int create_file(const char *filename, char *text_content)
 {
-int file;
-size_t bite = 0, len = _strlen(text_content);
-if (filename == NULL)
-{
-return (-1);
-}
-file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-if (file == -1)
-{
-return (-1);
-}
-if (len)
-{
-write(file, text_content, len);
-}
-close(file);
-return (bite == len ? -1 : 1);
+	int file;
+	int n, len = _strlen(text_content);
+	if (!filename)
+		return (-1);
+	if (!text_content)
+	{
+		file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+		if (file == -1)
+			return (-1);
+		close(file);
+		return (1);
+	}
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+    n = write(file, text_content, len);
+	if (n == -1)
+		return (-1);
+	close(file);
+	return (1);
 }
