3a723447b1a0a8296eb52b9dd9a5bd89b3bb7514
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-30 22:17:27 +0300

    0x15-file_io

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+    int file1, file2, file1_close, file2_close;
+    char *file_from;
+    char *file_to;
+    if (argc != 2)
+    {
+        dprintf(2, "Usage: cp file_from file_to\n");
+        return (97);
+    }
+    file_from = argv[1];
+    file_to = argv[2];
+    file1 = open(file_from, O_CREAT | O_RDONLY);
+    if (file1 == -1)
+    {
+        dprintf(2, "Error: Can't read from file %s\n", file_from);
+        return (98);
+    }
+    file2 = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 664);
+    if (file2 == -1)
+    {
+        dprintf(2, "Error: Can't write to %s\n", file_to);
+        return (99);
+    }
+    
+    file1_close = close(file1);
+    if (file1_close != 0)
+    {
+        dprintf(2, "Error: Can't close fd %i", file1_close);
+        return (100);
+    }
+    file2_close = close(file1);
+    if (file2_close != 0)
+    {
+        dprintf(2, "Error: Can't close fd %i", file2_close);
+        return (100);
+    }
+    return (0);
+}
