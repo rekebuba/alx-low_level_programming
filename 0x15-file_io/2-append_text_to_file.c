22dade4571edcf8d4d308eb05cf4ec4ba4b4e9b2
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-31 00:02:13 +0300

    0x15-file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -33,7 +33,7 @@ int append_text_to_file(const char *filename, char *text_content)
     {
         return (-1);
     }
-    write_file = (file, text_content, _strlen(text_content));
+    write_file = write(file, text_content, _strlen(text_content));
     if (write_file == -1)
     {
         return (-1);
