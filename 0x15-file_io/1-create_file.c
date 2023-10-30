a440b25ea2e68cfb92172f54097982fa88b7d642
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-31 00:32:40 +0300

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - find the length of the string
- * @s: the string
- * Return: int
- */
-int _strlen(char *s)
-{
-int count = 0;
-while (s[count] != '\0')
-{
-count++;
-}
-return (count);
-}
-
-/**
 * create_file - a function that creates a file
 * @filename: the name of the file to create
 * @text_content: a NULL terminated string to write to the file
@@ -26,24 +11,33 @@ return (count);
 */
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	int n, len = _strlen(text_content);
+	int fd;
+	int n, c = 0;
+
 	if (!filename)
 		return (-1);
+
 	if (!text_content)
 	{
-		file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-		if (file == -1)
+		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+		if (fd == -1)
 			return (-1);
-		close(file);
+		close(fd);
 		return (1);
 	}
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file == -1)
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
-    n = write(file, text_content, len);
+
+	while (text_content[c])
+		c++;
+
+	n = write(fd, text_content, c);
 	if (n == -1)
 		return (-1);
-	close(file);
+
+	close(fd);
+
 	return (1);
 }
