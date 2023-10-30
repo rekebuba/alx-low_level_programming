0f1fd015e97a4c629a08ae3a269eca6da5504b6d
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-30 23:49:03 +0300

    0x15-file_io

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+/**
+ * _strlen - find the length of the string
+ * @s: the string
+ * Return: int
+ */
+int _strlen(char *s)
+{
+int count = 0;
+while (s[count] != '\0')
+{
+count++;
+}
+return (count);
+}
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+    int file;
+    if (filename == NULL)
+    {
+        return (-1);
+    }
+    file = open(filename, O_RDONLY);
+    if (file = -1)
+    {
+        return (-1);
+    }
+    lseek(file, 0L, SEEK_END); /* Go to the end of the file*/
+    if (text_content == NULL)
+    {
+        return (1);
+    }
+    write(file, text_content, _strlen(text_content));
+    return (1);
+}
