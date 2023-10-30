66cae6b6f44633290e0293cae3982a087ac3a441
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-30 23:23:41 +0300

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
+void free_close(char **buf, int *fd1, int *fd2);
+
 /**
-* main - a program that copies the content of a file to anothe file
+* main - a program that copies the content of a file to another file
 * @argc: the number of cmd-line arguments
 * @argv: argv[1] (source file), argv[2] (destination file)
 * Return: returns (0) success, exits on error
 */
-
 int main(int argc, char *argv[])
 {
-    int file1, file2, file1_close, file2_close, to_read, to_write;
-    char *file_from;
-    char *file_to;
-    char *buffer;
+    int file1, file2, to_read, to_write;
+    char *file_from, *file_to, *buffer;
     if (argc != 3)
     {
-        dprintf(2, "Usage: cp file_from file_to\n");
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
         return (97);
     }
     file_from = argv[1];
@@ -47,17 +46,32 @@ int main(int argc, char *argv[])
         dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", file_to);
         return (99);
     }
-    file1_close = close(file1);
-    if (file1_close != 0)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file1_close);
-        return (100);
-    }
-    file2_close = close(file2);
-    if (file2_close != 0)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file2_close);
-        return (100);
-    }
-    return (to_write);
+    free_close(buffer, file1, file2);
+    return (0);
+}
+
+/**
+* free_close - free malloc'd memory and closes opened files
+* @buffer: pointer to the string buffer
+* @file1: pointer to the file1
+* @file2: pointer to file2
+* Return: returns nothing
+*/
+void free_close(char **buffer, int *file1, int *file2)
+{
+	int n, n1;
+
+	free(*buffer);
+	n = close(*file1);
+	if (n == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *file1);
+		exit(100);
+	}
+	n1 = close(*file2);
+	if (n1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *file2);
+		exit(100);
+	}
 }
