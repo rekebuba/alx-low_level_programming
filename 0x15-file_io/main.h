f7a498f5fbd20a24e6f7ecb39929c3f7a8031a7b
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-11-01 00:00:20 +0300

    0x15-file_io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef _FILE_IO_
-#define _FILE_IO_
+#ifndef _FILE_IO_HELLO
+#define _FILE_IO_HELLO
 
 #include <stdio.h>
 #include <fcntl.h>
@@ -8,10 +8,24 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdint.h>
+#include <fcntl.h>
+#include <elf.h>
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(char *s);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
 
 #endif
