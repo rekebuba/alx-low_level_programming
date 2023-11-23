bc178369fa03fc81f89f8afc5dc6e1869c4193f1
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-11-23 15:59:49 +0300

    0x14-bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * print_binary - prints the binary representation of a number.
+ *
+ * @n: unsigned long int
+ * Return: void
+ */
+
 void print_binary(unsigned long int n)
 {
     char buffer[64];
@@ -14,7 +21,7 @@ void print_binary(unsigned long int n)
     while (n != 0)
     {
         buffer[index] = (n & 1) ? '1' : '0';
-        n = (n << 1);
+        n = (n >> 1);
         index++;
     }
     for (i = index - 1; i >= 0; i--)
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_bit - Get the bit object
+ * 
+ * @n:
+ * @index:
+ * Return: int 
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+    
+}
