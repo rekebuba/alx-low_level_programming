1d3da0a66b5247cbd3107fee80d88220f9411bd4
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-11-27 17:16:31 +0300

    0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * read_textfile -
+ *
+ * @filename:
+ * @letters:
+ * Return: ssize_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    ssize_t to_read, to_write;
+    char *buffer;
+    int file = open(filename, O_RDONLY);
+    if (filename == NULL || file == -1)
+    {
+        return (0);
+    }
+
+    buffer = malloc(sizeof(char) * letters);
+
+    to_read = read(file, buffer, letters);
+
+    to_write = write(STDOUT_FILENO, buffer, to_read);
+    if (to_read != to_write)
+        return (0);
+
+    free(buffer);
+    close(file);
+
+    return (to_read);
+}
