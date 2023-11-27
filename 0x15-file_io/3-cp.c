59d525e5fd79982d454e49b08d21596126b7c491
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-11-27 22:48:08 +0300

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
-#define BUFFER 1024
-
+/**
+* main - a program that copies the content of a file to another file
+* @argc: the number of cmd-line arguments
+* @argv: argv[1] (source file), argv[2] (destination file)
+* Return: returns (0) success, exits on error
+*/
 int main(int argc, char *argv[])
 {
     char *file_from = argv[1];
@@ -13,13 +17,18 @@ int main(int argc, char *argv[])
         dprintf(2, "Usage: cp file_from file_to\n");
         exit(97);
     }
-    if (!file_from)
+    file_1 = open(file_from, O_RDONLY);
+    if (file_1 == -1)
     {
         dprintf(2, "Error: Can't read from file %s\n", file_from);
         exit(98);
     }
-    file_1 = open(file_from, O_RDONLY);
-
+    file_2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+    if (file_2 == -1)
+    {
+        dprintf(2, "Error: Can't write to %s\n", file_to);
+        exit(99);
+    }
     buffer = malloc(sizeof(char) * BUFFER);
     read_from = read(file_1, buffer, BUFFER);
     if (read_from == -1)
@@ -27,30 +36,30 @@ int main(int argc, char *argv[])
         dprintf(2, "Error: Can't read from file %s\n", file_from);
         exit(98);
     }
-    file_2 = open(file_to, O_CREAT | O_WRONLY | O_APPEND, 0664);
-    if (file_2 == -1)
-    {
-        dprintf(2, "Error: Can't write to %s\n", file_to);
-        exit(99);
-    }
     write_to = write(file_2, buffer, BUFFER);
     if (write_to == -1)
     {
         dprintf(2, "Error: Can't write to %s\n", file_to);
         exit(99);
     }
-    free(buffer);
-    close(file_1);
-    close(file_2);
-    if (file_1 == -1)
-    {
-        dprintf(2, "Error: Can't close fd %s\n", file_from);
-        exit(100);
-    }
-    else if (file_2 == -1)
-    {
-        dprintf(2, "Error: Can't close fd %s\n", file_to);
-        exit(100);
-    }
-    return (0);
+    free_close(buffer, file_1, file_2);
+}
+
+void free_close(char **buf, int *fd1, int *fd2)
+{
+	int n, n1;
+
+	free(*buf);
+	n = close(*fd1);
+	if (n == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *fd1);
+		exit(100);
+	}
+	n1 = close(*fd2);
+	if (n1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *fd2);
+		exit(100);
+	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef FILE_IO
 #define FILE_IO
 
+#define BUFFER 1024
+
 #include <string.h>
 #include <stdio.h>
 #include <fcntl.h>
@@ -15,6 +17,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+void free_close(char **buf, int *fd1, int *fd2);
 
 
 #endif
