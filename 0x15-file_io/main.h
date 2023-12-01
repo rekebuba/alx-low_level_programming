9873e75dd483c5fd3fa3c711a48728c6f3c35e88
rekebuba <abukiabdullahi1@gmail.com>
2023-12-01 13:45:36 -0800

    0x15-file_io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -23,5 +23,15 @@ void free_close(char **buf, int *fd1, int *fd2);
 void print_error(const char *message);
 void print_elf_header(const Elf64_Ehdr *header);
 
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif
