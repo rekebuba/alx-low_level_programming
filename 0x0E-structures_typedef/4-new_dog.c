5b46afb27b6d4e10ab0a2c7b6ee4d5734412d352
rekebuba <abukiabdullahi1@gmail.com>
2023-12-16 17:05:11 +0300

    0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,12 +7,13 @@
  */
 int _strlen(char *word)
 {
-int i = 0;
-while (word[i] != '\0')
-{
-i++;
-}
-return (i);
+	int i = 0;
+
+	while (word[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 /**
  * _strcopy - copy the string
@@ -21,11 +22,12 @@ return (i);
  */
 void _strcopy(char *word1, char *word2)
 {
-int i;
-for (i = 0; i < _strlen(word2); i++)
-{
-word1[i] = word2[i];
-}
+	int i;
+
+	for (i = 0; i < _strlen(word2); i++)
+	{
+		word1[i] = word2[i];
+	}
 }
 
 /**
@@ -35,26 +37,26 @@ word1[i] = word2[i];
  * @owner: owner
  * Return: dog_t
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *ptr;
-ptr = malloc(sizeof(dog_t));
-if (ptr == NULL)
-return (NULL);
-ptr->name = malloc(sizeof(char) * (_strlen(name) + 1));
-ptr->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-if (ptr->name == NULL || ptr->owner == NULL)
-{
-free(ptr);
-free(ptr->name);
-free(ptr->owner);
-return (NULL);
-}
-_strcopy(ptr->name, name);
-_strcopy(ptr->owner, owner);
-ptr->name[_strlen(name)] = '\0';
-ptr->owner[_strlen(owner)] = '\0';
-ptr->age = age;
-return (ptr);
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	ptr->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (ptr->name == NULL || ptr->owner == NULL)
+	{
+		free(ptr);
+		free(ptr->name);
+		free(ptr->owner);
+		return (NULL);
+	}
+	_strcopy(ptr->name, name);
+	_strcopy(ptr->owner, owner);
+	ptr->name[_strlen(name)] = '\0';
+	ptr->owner[_strlen(owner)] = '\0';
+	ptr->age = age;
+	return (ptr);
 }
