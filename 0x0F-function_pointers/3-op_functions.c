db168a7f55130905524187f56214cdd4bda6e77d
rekebuba <abukiabdullahi1@gmail.com>
2023-12-16 17:08:51 +0300

    0x0F-function_pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -15,7 +15,7 @@ int op_mod(int a, int b);
  */
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
 /**
  * op_sub - Returns the difference of two numbers.
@@ -26,7 +26,7 @@ return (a + b);
  */
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
 /**
  * op_mul - Returns the product of two numbers.
@@ -36,7 +36,7 @@ return (a - b);
  */
 int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
 /**
  * op_div - Returns the division of two numbers.
@@ -46,7 +46,7 @@ return (a * b);
  */
 int op_div(int a, int b)
 {
-return (a / b);
+	return (a / b);
 }
 /**
  * op_mod - Returns the remainder of the division of two numbers.
@@ -56,5 +56,5 @@ return (a / b);
  */
 int op_mod(int a, int b)
 {
-return (a % b);
+	return (a % b);
 }
