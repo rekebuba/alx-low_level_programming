28b3e642166099c138920687f2a4f34669a96b61
rekebuba <abukiabdullahi1@gmail.com>
2024-01-11 11:26:06 +0300

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,5 +8,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    
+	size_t count = 0;
+	const dlistint_t *temp = h;
+	while (temp != NULL)
+	{
+		printf("%d \n", temp->n);
+		count++;
+		temp = temp->next;
+	}
+	return count;
 }
---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked  list.
+ * 
+ * @h: double node
+ * Return: size_t
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t count = 0;
+    const dlistint_t *temp = h;
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
