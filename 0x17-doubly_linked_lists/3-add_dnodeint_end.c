b2af860eb9c8dd480bacd15eee9825e07e53d184
rekebuba <abukiabdullahi1@gmail.com>
2024-01-11 15:55:14 +0300

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,16 +9,31 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *ptr = *head;
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+	dlistint_t *ptr = *head;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
+	if (new_node == NULL)
+		return (NULL);
 
-    while (ptr != NULL)
-    {
-        ptr = ptr->next;
-    }
+	if (ptr == NULL)
+	{
+		new_node->n = n;
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
 
-    
+	while (ptr->next != NULL)
+	{
+		ptr = ptr->next;
+	}
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	ptr->next = new_node;
+	new_node->prev = ptr;
+
+	return (new_node);
 }
