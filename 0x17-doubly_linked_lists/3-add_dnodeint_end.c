6d276ea5035d6942dfeb7b55d813cbdd2f1a25ae
rekebuba <abukiabdullahi1@gmail.com>
2024-01-11 15:45:24 +0300

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,1 +1,24 @@
 
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add node at the end
+ * @head: head pointer
+ * @n: data to insert
+ * Return: final list
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+    dlistint_t *ptr = *head;
+    dlistint_t *new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+
+    while (ptr != NULL)
+    {
+        ptr = ptr->next;
+    }
+
+    
+}
