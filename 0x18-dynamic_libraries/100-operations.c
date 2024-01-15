60b8d6a47c862eaf6a33604d81874ecaff2e9425
rekebuba <abukiabdullahi1@gmail.com>
2024-01-15 15:46:11 +0300

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+/**
+ * add - adds two numbers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - module of two numbers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
