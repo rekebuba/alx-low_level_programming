8f8c616b7aab98adc4f89b25f3dea163678ac8ef
rekebuba <abukiabdullahi1@gmail.com>
2024-02-01 18:12:08 +0300

    0x1A-hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,6 +7,6 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *array = malloc(sizeof(hash_table_t) * size); 
+    hash_table_t *array = malloc(sizeof(hash_node_t) * size); 
     return (array);
 }
