84dfa328c8a9de9b4cf8eb02053f432d18a811e7
rekebuba <abukiabdullahi1@gmail.com>
2024-02-01 20:20:01 +0300

    0x1A-hash_tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,14 +9,14 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long a;
+	unsigned long index;
 
 	if (key == NULL)
 		return (0);
 
-	a = key_index((unsigned char *)key, 1024);
+	index = key_index((unsigned char *)key, ht->size);
 
-	if (!add_nodeint(&ht->array[a], key, value))
+	if (!add_nodeint(&ht->array[index], key, value))
 		return (0);
 
 	return (1);
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ *
+ * @ht: the hash table to look into
+ * @key: the key to look for
+ * Return: the value or NULL if key couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	if (key == NULL)
+	{
+		return (NULL);
+	}
+	index = key_index((unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
+		return (NULL);
+	return (ht->array[index]->value);
+}
