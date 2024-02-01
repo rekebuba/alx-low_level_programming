9e870fd819caafc2ea7258f2d02f1ff82b4a544c
rekebuba <abukiabdullahi1@gmail.com>
2024-02-01 20:00:54 +0300

    0x1A-hash_tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,43 +2,45 @@
 
 /**
  * hash_table_set - adds an element to the hash table
- *
- * ht: hash table you want to add or update the key/value to
- * key: is the key
- * value: associated with the key
- * @return int
+ * @ht: hash table you want to add or update the key/value to
+ * @key: is the key
+ * @value: associated with the key
+ * Return: 1 if succeeded 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long a;
-    if (key == NULL)
-        return (0);
+	unsigned long a;
 
-    a = key_index((unsigned char *)key, 1024);
+	if (key == NULL)
+		return (0);
 
-    if (!add_nodeint(&ht->array[a], key, value))
-        return (0);
+	a = key_index((unsigned char *)key, 1024);
 
-    return (1);
+	if (!add_nodeint(&ht->array[a], key, value))
+		return (0);
+
+	return (1);
 }
 
 /**
  * add_nodeint - adds a new node at the beginning
+ * @head: head of the node
  * @key: the key
  * @value: - the value
- * Return: listint_t* (new_node)
+ * Return: new_node
  */
-hash_node_t *add_nodeint(hash_node_t **head, const char *key, const char *value)
+hash_node_t *add_nodeint(hash_node_t **head, const char *key,
+						 const char *value)
 {
-    hash_node_t *new_nod = malloc(sizeof(hash_node_t));
+	hash_node_t *new_nod = malloc(sizeof(hash_node_t));
 
-    if (new_nod == NULL)
-    {
-        return (NULL);
-    }
-    new_nod->key = (char *)key;
-    new_nod->value = (char *)value;
-    new_nod->next = *head;
-    *head = new_nod;
-    return (new_nod);
+	if (new_nod == NULL)
+	{
+		return (NULL);
+	}
+	new_nod->key = (char *)key;
+	new_nod->value = (char *)value;
+	new_nod->next = *head;
+	*head = new_nod;
+	return (new_nod);
 }
