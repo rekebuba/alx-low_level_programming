790fc1efc4a1680518ed9e71afadac6ae04c99b4
rekebuba <abukiabdullahi1@gmail.com>
2024-05-09 15:00:27 +0300

    0x1E-search_algorithms

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,5 +1,9 @@
 #include "search_algos.h"
-#include <math.h>
+
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
+
 
 /**
  * jump_list - A function that searches for a value
@@ -23,25 +27,23 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 	{
 		i = 0;
 		for (high = low; i != jump && high->next != NULL; i++)
-		{
 			high = high->next;
-		}
+
 		printf("Value checked at index [%ld] = [%d]\n", high->index, high->n);
+
 		if (low->n <= value && high->n >= value)
-		{
-			printf("Value found between indexes [%ld] and [%ld]\n",
-																low->index, high->index);
 			break;
-		}
-		low = high;
+		else if (high->next != NULL)
+			low = high;
 	}
-	for (; low->next != NULL && low->index <= high->index; low = low->next)
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+														low->index, high->index);
+	for (; low != NULL && low->index <= high->index; low = low->next)
 	{
 		printf("Value checked at index [%ld] = [%d]\n", low->index, low->n);
 		if (low->n == value)
-		{
 			return (low);
-		}
 	}
 
 	return (NULL);
